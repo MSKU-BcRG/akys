003f42402507530d740bbd6baba392c1fd47ae7b
cmldk <cemal.dak@gmail.com>
2020-09-08 21:07:15 +0300

    Initial commit

--- expressjs/middleware/hasAuth.js
+++
@@ -1,8 +0,0 @@
-let createError = require('http-errors')
-
-exports.isLoggedIn = function (req, res, next) {
-    if (req.user)
-        next();
-    else
-        next(createError(404, 'Page does not exist.'));
-}
